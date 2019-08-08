290ea988eb54ff712897277aeb0f2d20251c0a49
Maxim Belyaev <mabelyaev2@gmail.com>
2019-08-08 09:57:18 +0300

    final update

--- js/common.js
+++ js/common.js
@@ -1,14 +1,46 @@
-const subForm = document.querySelector('.submit-form')
+document.addEventListener('DOMContentLoaded', () => {
 
+  const subscriptionForms = document.querySelectorAll('.submit-form');
+  const hamburgerBtn = document.querySelector('.hamburger');
+  const mobileNav = document.querySelector('.test-mobile-nav');
 
+  const validateEmail = (email) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
 
-subForm.addEventListener('submit', (e) => {
-	if (!subForm.value) {
-		alert('error')
-	}
+  subscriptionForms.forEach((form) => {
+
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const emailInput = e.target.querySelector('.submit-form__input');
+      const formID = parseInt(form.dataset.formId);
+      const data = new FormData(form);
+      const email = data.get('email').trim();
+
+      if (!validateEmail(email)) {
+        emailInput.classList.add('validation-error');
+        emailInput.placeholder = 'Not valid info';
+        return;
+      } else {
+        emailInput.classList.remove('validation-error');
+        emailInput.placeholder = 'Your email address';
+        alert(JSON.stringify({
+          formID,
+          email,
+        }));
+
+        emailInput.value = '';
+      }
+    });
+
+  });
+
+  hamburgerBtn.addEventListener('click', () => {
+    hamburgerBtn.classList.toggle('is-active');
+    mobileNav.classList.toggle('menu-hidden');
+  });
+
 });
-	
-
-
-
 
